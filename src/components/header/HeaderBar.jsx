8e0025ef1bcb1b80641d4f0938488a90ea317788
Jérémy <jeremy.vaneste@free.fr>
2023-01-20 19:45:38 +0100

    update header

--- src/components/header/HeaderBar.jsx
+++ src/components/header/HeaderBar.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import "./header_bar.css";
+import {
+  HeaderBarContainer,
+  LeftPartContainer,
+  MiddlePartContainer,
+  OptionsItem,
+  RightPartContainer,
+} from "./header_styled";
+import { SiVisualstudiocode } from "react-icons/si";
 
 const HeaderBar = () => {
   return (
-    <div className="headerBarContainer">
-      <p>HeaderBar</p>
-    </div>
+    <HeaderBarContainer>
+      <LeftPartContainer>
+        <SiVisualstudiocode color="#2660A4" size={18}></SiVisualstudiocode>
+        <OptionsItem>File</OptionsItem>
+        <OptionsItem>Edit</OptionsItem>
+        <OptionsItem>Selection</OptionsItem>
+        <OptionsItem>View</OptionsItem>
+        <OptionsItem>Go</OptionsItem>
+        <OptionsItem>Run</OptionsItem>
+        <OptionsItem>Terminal</OptionsItem>
+        <OptionsItem>Help</OptionsItem>
+      </LeftPartContainer>
+
+      <MiddlePartContainer></MiddlePartContainer>
+
+      <RightPartContainer></RightPartContainer>
+    </HeaderBarContainer>
   );
 };
 
---
+++ src/components/header/header_styled.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const HeaderBarContainer = styled.div`
+  background-color: yellow;
+  display: flex;
+  align-items: center;
+  height: 4vh;
+`;
+
+export const LeftPartContainer = styled.div`
+  display: flex;
+`;
+
+export const OptionsItem = styled.p`
+  color: dark;
+`;
+
+export const MiddlePartContainer = styled.div``;
+
+export const RightPartContainer = styled.div``;
