9cd8c932cbe8d92a4f993dba0aecc94e34395491
Jérémy <jeremy.vaneste@free.fr>
2023-01-22 15:03:51 +0100

    sidebar icons display => ok

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,56 @@
 import React from "react";
-import { SidebarContainer } from "./sidebar_styled";
+import {
+  BottomIconsContainer,
+  IconContainer,
+  SidebarContainer,
+  TopIconsContainer,
+} from "./sidebar_styled";
+import {
+  VscCopy,
+  VscSearch,
+  VscSourceControl,
+  VscRemoteExplorer,
+  VscDebugAlt,
+  VscExtensions,
+  VscLiveShare,
+  VscGithubAlt,
+  VscAccount,
+  VscSettingsGear,
+} from "react-icons/vsc";
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
-      <p>Sidebar</p>
+      <TopIconsContainer>
+        <IconContainer>
+          <VscCopy className="topIcon" size={30}></VscCopy>
+        </IconContainer>
+        <IconContainer>
+          <VscSearch className="topIcon" size={30}></VscSearch>
+        </IconContainer>
+        <IconContainer>
+          <VscSourceControl className="topIcon" size={30}></VscSourceControl>
+        </IconContainer>
+        <IconContainer>
+          <VscRemoteExplorer className="topIcon" size={30}></VscRemoteExplorer>
+        </IconContainer>
+        <IconContainer>
+          <VscDebugAlt className="topIcon" size={30}></VscDebugAlt>
+        </IconContainer>
+        <IconContainer>
+          <VscExtensions className="topIcon" size={30}></VscExtensions>
+        </IconContainer>
+        <IconContainer>
+          <VscLiveShare className="topIcon" size={30}></VscLiveShare>
+        </IconContainer>
+        <IconContainer>
+          <VscGithubAlt className="topIcon" size={30}></VscGithubAlt>
+        </IconContainer>
+      </TopIconsContainer>
+      <BottomIconsContainer>
+        <VscAccount size={30}></VscAccount>
+        <VscSettingsGear size={30}></VscSettingsGear>
+      </BottomIconsContainer>
     </SidebarContainer>
   );
 };
--- src/components/sidebar/sidebar_styled.jsx
+++ src/components/sidebar/sidebar_styled.jsx
@@ -3,4 +3,41 @@ import styled from "styled-components";
 export const SidebarContainer = styled.div`
   background-color: purple;
   width: 4%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const TopIconsContainer = styled.div`
+  background-color: green;
+  height: 85%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const IconContainer = styled.div`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  align-items: center;
+  background-color: purple;
+  border-left: 2px solid black;
+
+  & .topIcon {
+    background-color: red;
+    width: 100%;
+    height: 60%;
+  }
+`;
+
+export const BottomIconsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 15%;
+
+  background-color: blue;
 `;
