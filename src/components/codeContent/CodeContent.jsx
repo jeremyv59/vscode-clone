482fe91f8310f704f6a53e36b8adc19210dcc818
Jérémy <jeremy.vaneste@free.fr>
2023-01-24 15:54:10 +0100

    create launch background

--- src/components/codeContent/CodeContent.jsx
+++ src/components/codeContent/CodeContent.jsx
@@ -1,28 +1,116 @@
 import React, { useContext } from "react";
 import { FileContext } from "../../context/FileContext.jsx";
 import { FilesArray } from "../../data/FileData.jsx";
+import { SiVisualstudiocode } from "react-icons/si";
 import Splitter from "../splitter/Splitter.jsx";
-import { CodeContentContainer } from "./code_content.jsx";
+import {
+  BackgroundIconContainer,
+  CodeContentContainer,
+  ContainerKeyboardItem,
+  ContainerOptionText,
+  KeyboardItem,
+  OptionContainer,
+  OptionText,
+  PlusSign,
+} from "./code_content.jsx";
 import Highlight from "react-highlight";
-
-var hljs = require("highlight.js");
 
 const CodeContent = () => {
   const context = useContext(FileContext);
 
+  const { selectedFile } = context;
+
   return (
     <CodeContentContainer>
       <Splitter flexDirectionValue={"column"} directionValue={"vertical"}>
-        <div style={{ backgroundColor: "green", maxHeight: "100%" }}>
-          {FilesArray.map((file) => {
-            return file.fileName === context.selectedFile ? (
-              <p key={file.id}>
-                <Highlight language={file.language}>{file.content}</Highlight>
-              </p>
-            ) : (
-              ""
-            );
-          })}
+        <div
+          style={{
+            backgroundColor: "green",
+            maxHeight: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {selectedFile ? (
+            FilesArray.map((file) => {
+              return file.fileName === context.selectedFile ? (
+                <p key={file.id}>
+                  <Highlight language={file.language}>{file.content}</Highlight>
+                </p>
+              ) : (
+                ""
+              );
+            })
+          ) : (
+            <React.Fragment>
+              <SiVisualstudiocode
+                style={{
+                  color: "yellow",
+                  width: "100%",
+                  height: "50%",
+                  marginBottom: "2rem",
+                }}
+              ></SiVisualstudiocode>
+              <OptionContainer>
+                <ContainerOptionText>
+                  <OptionText>Show All Commands</OptionText>
+                </ContainerOptionText>
+                <ContainerKeyboardItem>
+                  <KeyboardItem>Ctrl</KeyboardItem>
+                  <PlusSign>+</PlusSign>
+                  <KeyboardItem>Shift</KeyboardItem>
+                  <PlusSign>+</PlusSign>
+                  <KeyboardItem>Ctrl</KeyboardItem>
+                </ContainerKeyboardItem>
+              </OptionContainer>
+
+              <OptionContainer>
+                <ContainerOptionText>
+                  <OptionText>Go to file</OptionText>
+                </ContainerOptionText>
+                <ContainerKeyboardItem>
+                  <KeyboardItem>Ctrl</KeyboardItem>
+                  <PlusSign>+</PlusSign>
+                  <KeyboardItem>P</KeyboardItem>
+                </ContainerKeyboardItem>
+              </OptionContainer>
+
+              <OptionContainer>
+                <ContainerOptionText>
+                  <OptionText>Find in files</OptionText>
+                </ContainerOptionText>
+                <ContainerKeyboardItem>
+                  <KeyboardItem>Ctrl</KeyboardItem>
+                  <PlusSign>+</PlusSign>
+                  <KeyboardItem>Shift</KeyboardItem>
+                  <PlusSign>+</PlusSign>
+                  <KeyboardItem>Ctrl</KeyboardItem>
+                </ContainerKeyboardItem>
+              </OptionContainer>
+
+              <OptionContainer>
+                <ContainerOptionText>
+                  <OptionText>Start debugging</OptionText>
+                </ContainerOptionText>
+                <ContainerKeyboardItem>
+                  <KeyboardItem>F5</KeyboardItem>
+                </ContainerKeyboardItem>
+              </OptionContainer>
+
+              <OptionContainer>
+                <ContainerOptionText>
+                  <OptionText>Toggle terminal</OptionText>
+                </ContainerOptionText>
+                <ContainerKeyboardItem>
+                  <KeyboardItem>Ctrl</KeyboardItem>
+                  <PlusSign>+</PlusSign>
+                  <KeyboardItem>ù</KeyboardItem>
+                </ContainerKeyboardItem>
+              </OptionContainer>
+            </React.Fragment>
+          )}
         </div>
         <div style={{ backgroundColor: "purple", width: "100%" }}>
           <p>Terminal Div</p>
--- src/components/codeContent/code_content.jsx
+++ src/components/codeContent/code_content.jsx
@@ -1,9 +1,51 @@
 import styled from "styled-components";
+import { GlobalTheme } from "../../theme/GlobalTheme";
 
 export const CodeContentContainer = styled.div`
   background-color: orange;
   min-width: 20%;
+  width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+`;
+
+export const OptionContainer = styled.div`
+  background-color: orange;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 0.5rem;
+`;
+
+export const ContainerOptionText = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const ContainerKeyboardItem = styled.div`
+  display: flex;
+  width: 50%;
+`;
+
+export const OptionText = styled.p`
+  color: ${GlobalTheme.colors.lightGray};
+  font-size: 13px;
+  margin-right: 1rem;
+`;
+
+export const KeyboardItem = styled.p`
+color:${GlobalTheme.colors.white};
+font-size 13px;
+padding : 0.2rem 0.4rem;
+border-radius : 4px;
+border-bottom : 1px solid black;
+background : ${GlobalTheme.colors.gray}
+`;
+
+export const PlusSign = styled.p`
+  color: ${GlobalTheme.colors.white};
+  margin: 0rem 0.3rem;
 `;
