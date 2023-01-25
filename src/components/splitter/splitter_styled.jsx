787a83fc140a8a758b4f51d7e820ee06010d3b9d
Jérémy <jeremy.vaneste@free.fr>
2023-01-25 02:31:15 +0100

    update gutter style

--- src/components/splitter/splitter_styled.jsx
+++ src/components/splitter/splitter_styled.jsx
@@ -1,5 +1,6 @@
 import Split from "react-split";
 import styled from "styled-components";
+import { GlobalTheme } from "../../theme/GlobalTheme";
 
 export const CustomSplit = styled(Split)`
   display: flex;
@@ -10,5 +11,11 @@ export const CustomSplit = styled(Split)`
   & .gutter:hover {
     cursor: ${(props) =>
       props.flexDirValue === "row" ? "col-resize" : "row-resize"};
+  }
+
+  & .gutter {
+    background-color: ${GlobalTheme.colors.white};
+    background-repeat: no-repeat;
+    background-position: 0%;
   }
 `;
